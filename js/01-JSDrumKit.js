5e37db6efd3599710a41f030e620acaee062532b
William Chu <Williamchu51@gmail.com>
2018-04-13 20:22:42 -0700

    refactor js

--- js/01-JSDrumKit.js
+++ js/01-JSDrumKit.js
@@ -1,14 +1,21 @@
-document.addEventListener('keydown', function(event) {
+function playSound(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  key.classList.add('highlight');
   if(!audio) {
     return;
   } else {
     audio.currentTime = 0;
     audio.play();
   }
+}
+
+function keyPress(event) {
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  key.classList.add('highlight');
   document.addEventListener('keyup', function(event) {
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     key.classList.remove('highlight');
   });
-});
+}
+
+document.addEventListener('keydown', playSound);
+document.addEventListener('keydown', keyPress);
